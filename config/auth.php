69e7da3386445f2866eb988172e9d49181156e05
almant12 <dokoalmant123@gmail.com>
2024-10-15 20:28:11 +0200

    auth by sanctum token

--- config/auth.php
+++ config/auth.php
@@ -36,15 +36,15 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-        'api'=>[
-            'driver' => 'sanctum',
-            'provider' => 'users'
-        ]
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
     ],
+    'api' => [
+        'driver' => 'sanctum',
+        'provider' => 'users',
+    ],
+],
 
     /*
     |--------------------------------------------------------------------------
