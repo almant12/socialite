476d7628d14d9a6614fa512c91afcaefac177bb5
almant12 <dokoalmant123@gmail.com>
2024-10-17 20:44:04 +0200

    change middleware for auth sanctum

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -34,8 +34,6 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
-
         $token = $user->createToken('api-token');
 
         return response()->json([
